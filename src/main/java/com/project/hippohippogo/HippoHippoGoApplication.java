a88b7ddd92efec68350e1d972640b06f78ea3fdb
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-10 11:30:32 +0200

    Initial Commit

---
+++ src/main/java/com/project/hippohippogo/HippoHippoGoApplication.java
@@ -0,0 +1,13 @@
+package com.project.hippohippogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippoHippoGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HippoHippoGoApplication.class, args);
+    }
+
+}
