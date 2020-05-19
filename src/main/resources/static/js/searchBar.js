a28dfe55c62c7622dc016ea6e9461078ba6ab661
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-20 01:17:40 +0200

    Route Suggestion Mechanism to Suggestions Controller

--- src/main/resources/static/js/searchBar.js
+++ src/main/resources/static/js/searchBar.js
@@ -6,7 +6,6 @@ $(function () {
     const inputField = $('#search_form_input_homepage');
     const clearButton = $('#search_form_input_clear');
     clearButton.hide();
-    console.log("main hide")
     clearButton.on('click', function () {
         inputField.val("");
         clearButton.hide();
@@ -22,26 +21,8 @@ $(function () {
 
     /////////////////////////////////////// Suggestions //////////////////////////////////////
 
-    const countriesArray = $.map(countries, function (value, key) {
-        return {value: value, data: key};
-    });
-
-    // Initialize ajax autocomplete:
     inputField.autocomplete({
-        // serviceUrl: '/autosuggest/service/url',
-        lookup: countriesArray,
-        lookupFilter: function (suggestion, originalQuery, queryLowerCase) {
-          console.log(suggestion, originalQuery, queryLowerCase);
-          const re = new RegExp('\^' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
-          console.log(re);
-          return re.test(suggestion.value);
-        },
-        onSelect: function (suggestion) {
-            console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
-        },
-        onInvalidateSelection: function () {
-            console.log('You selected: none');
-        },
+        serviceUrl: '/suggestions',
         lookupLimit: 8
     });
 });
