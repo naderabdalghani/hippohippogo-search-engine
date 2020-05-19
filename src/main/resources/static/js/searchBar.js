f892875b79f23b188d66b57313c4759344b1b448
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-19 21:30:45 +0200

    Add Suggestion Mechanism with Dummy Data

--- src/main/resources/static/js/searchBar.js
+++ src/main/resources/static/js/searchBar.js
@@ -1,21 +1,48 @@
 /*<![CDATA[*/
 $(function () {
-  const inputField = $('#search_form_input_homepage');
-  const clearButton = $('#search_form_input_clear');
-  clearButton.hide();
-  console.log("main hide")
-  clearButton.on('click',function () {
-    inputField.val("");
+
+    /////////////////////////////////////// Clear Text ///////////////////////////////////////
+
+    const inputField = $('#search_form_input_homepage');
+    const clearButton = $('#search_form_input_clear');
     clearButton.hide();
-    inputField.trigger('focus');
-  });
-  inputField.on('change keyup paste', function(){
-    if (inputField.val() !== "") {
-      clearButton.show();
-    }
-    else {
-      clearButton.hide();
-    }
-  });
+    console.log("main hide")
+    clearButton.on('click', function () {
+        inputField.val("");
+        clearButton.hide();
+        inputField.trigger('focus');
+    });
+    inputField.on('change keyup paste', function () {
+        if (inputField.val() !== "") {
+            clearButton.show();
+        } else {
+            clearButton.hide();
+        }
+    });
+
+    /////////////////////////////////////// Suggestions //////////////////////////////////////
+
+    const countriesArray = $.map(countries, function (value, key) {
+        return {value: value, data: key};
+    });
+
+    // Initialize ajax autocomplete:
+    inputField.autocomplete({
+        // serviceUrl: '/autosuggest/service/url',
+        lookup: countriesArray,
+        lookupFilter: function (suggestion, originalQuery, queryLowerCase) {
+          console.log(suggestion, originalQuery, queryLowerCase);
+          const re = new RegExp('\^' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
+          console.log(re);
+          return re.test(suggestion.value);
+        },
+        onSelect: function (suggestion) {
+            console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
+        },
+        onInvalidateSelection: function () {
+            console.log('You selected: none');
+        },
+        lookupLimit: 8
+    });
 });
 /*]]>*/
