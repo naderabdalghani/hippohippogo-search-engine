dd0255234f5af5594f6ea998e6e79787af5c6f10
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-20 05:19:16 +0200

    Add Search Bar Voice Input Icon Dynamic Styling

--- src/main/resources/static/js/searchBar.js
+++ src/main/resources/static/js/searchBar.js
@@ -1,10 +1,12 @@
 /*<![CDATA[*/
 $(function () {
 
-    /////////////////////////////////////// Clear Text ///////////////////////////////////////
-
     const inputField = $('#search_form_input');
     const clearButton = $('#search_form_input_clear');
+    const voiceInputButton = $('#search_form_voice_input');
+
+    /////////////////////////////////////// Clear Text ///////////////////////////////////////
+
     clearButton.hide();
     clearButton.on('click', function () {
         inputField.val("");
@@ -12,15 +14,7 @@ $(function () {
         inputField.trigger('focus');
         inputField.autocomplete().clear();
     });
-    clearButton.on('mousedown', function () {
-        clearButton.css("color", "red");
-    });
-    clearButton.on('mouseover', function () {
-        clearButton.css("color", "#616161");
-    });
-    clearButton.on('mouseup mouseout', function () {
-        clearButton.css("color", "#aaa");
-    });
+
     inputField.on('change keyup paste', function () {
         if (inputField.val() !== "") {
             clearButton.show();
@@ -35,6 +29,28 @@ $(function () {
         serviceUrl: '/suggestions',
         lookupLimit: 8,
         noCache: true
+    });
+
+    ///////////////////////////////////////// Styling ////////////////////////////////////////
+
+    clearButton.on('mousedown', function () {
+        clearButton.css("color", "red");
+    });
+    clearButton.on('mouseover', function () {
+        clearButton.css("color", "#616161");
+    });
+    clearButton.on('mouseup mouseout', function () {
+        clearButton.css("color", "#aaa");
+    });
+
+    voiceInputButton.on('mousedown', function () {
+        voiceInputButton.css("color", "blue");
+    });
+    voiceInputButton.on('mouseover', function () {
+        voiceInputButton.css("color", "#616161");
+    });
+    voiceInputButton.on('mouseup mouseout', function () {
+        voiceInputButton.css("color", "#aaa");
     });
 });
 /*]]>*/
