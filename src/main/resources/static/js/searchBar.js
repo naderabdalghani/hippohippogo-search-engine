f464b80f4e681102285b6b0fb8049199c96eb282
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-20 04:06:50 +0200

    Add Clear Text Button jQuery Styling

--- src/main/resources/static/js/searchBar.js
+++ src/main/resources/static/js/searchBar.js
@@ -12,6 +12,15 @@ $(function () {
         inputField.trigger('focus');
         inputField.autocomplete().clear();
     });
+    clearButton.on('mousedown', function () {
+        clearButton.css("color", "red");
+    });
+    clearButton.on('mouseover', function () {
+        clearButton.css("color", "#616161");
+    });
+    clearButton.on('mouseup mouseout', function () {
+        clearButton.css("color", "#aaa");
+    });
     inputField.on('change keyup paste', function () {
         if (inputField.val() !== "") {
             clearButton.show();
