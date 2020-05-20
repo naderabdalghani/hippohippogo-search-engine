9ebe4a2c30af3f2defc38f399cf8956129c67748
Muhanad <muhanad.atef23@gmail.com>
2020-05-21 01:41:17 +0200

    Optimize Page Ranking

--- src/main/java/com/project/hippohippogo/services/Ranker.java
+++ src/main/java/com/project/hippohippogo/services/Ranker.java
@@ -11,8 +11,8 @@ import java.util.*;
 
 @Service
 public class Ranker {
-    private final int pageRankIterations = 12; // Number of iterations on page ranks
-    float d = 0.5f; // Damping factor
+    private final int pageRankIterations = 20; // Number of iterations on page ranks
+    float d = 0.85f; // Damping factor
     private PagesConnectionRepository pagesConnection;
     private PageRankRepository pageRankRepository;
 
@@ -33,12 +33,14 @@ public class Ranker {
         List<PagesConnection> pageConnectionsArray = (List<PagesConnection>) pagesConnection.findAll();
         // Holding Page rank of each iteration
         HashMap<String,Float> pageRankHashTable = new HashMap<String,Float>();
-        // Temp map for page_rank table
+        // Temp map for page_rank table as a database table for page_rank table
         HashMap<String,PageRank> tempHashMap = new HashMap<String,PageRank>();
-        // Initialize pages in page_rank table
+        // Initialize pages in page_rank table with rank = 1
         for (PagesConnection p : pageConnectionsArray) {
             Optional<PageRank> pageRank1 = pageRankRepository.findById(p.getReferred());
             Optional<PageRank> pageRank2 = pageRankRepository.findById(p.getReferring());
+            // If we found that the page is added and we find it again in Referring column then outLinks++
+            // Else add the page to page_rank table
             if (pageRank2.isPresent()) {
                 pageRank2.get().setOut_links(pageRank2.get().getOut_links()+1);
                 pageRankRepository.save(pageRank2.get());
@@ -49,6 +51,7 @@ public class Ranker {
                 tempHashMap.put(pr.getPage(),pr);
                 pageRankHashTable.put(pr.getPage(),0f);
             }
+            // If the page was in the referred column and it wasn't added before then add it to page_rank table
             if (pageRank1.isPresent()) {
                 continue;
             } else {
@@ -59,27 +62,32 @@ public class Ranker {
             }
         }
 
-        List<PageRank> pageRankList = pageRankRepository.findAll();
+        List<PageRank> pageRankList = pageRankRepository.findAll(); // Holding pages in Page Rank table to iterate on it and get the rank value for each page in it
         // This loop is used to iterate on page ranks and update them
+        // For each loop we calculate PR(A) = (d-1)+d*(PR(B)/C(B)+...+PR(N)/C(N)
         for (int i=0;i<pageRankIterations;i++) {
+            // Initialize each page rank with (1-d) at the beginning
             pageRankHashTable.replaceAll((k,v) -> (1-d));
+            // Then iterate and when finding a page refer to another page update referred page rank value
             for (PagesConnection p : pageConnectionsArray) {
                 PageRank pr = tempHashMap.get(p.getReferring());
                 pageRankHashTable.put(p.getReferred(),pageRankHashTable.get(p.getReferred())+d*(pr.getRank()/pr.getOut_links()));
             }
 
-            // Updating temp hash map
+            // Updating temp hash map with the last calculated values
             for (PageRank p : pageRankList) {
                 p.setRank(pageRankHashTable.get(p.getPage()));
                 tempHashMap.put(p.getPage(),p);
             }
         }
 
-        // Updating database
+        // Updating database with the page rank values from the last iteration
         for (PageRank p : pageRankList) {
             p.setRank(tempHashMap.get(p.getPage()).getRank());
             pageRankRepository.save(p);
         }
 
     }
+
+    
 }
