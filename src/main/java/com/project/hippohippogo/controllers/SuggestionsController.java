379bf0f265453c0067c00d86bcc82d513b2c0763
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-20 02:51:34 +0200

    Sort Suggestions by Number of Hits (Popularity)

--- src/main/java/com/project/hippohippogo/controllers/SuggestionsController.java
+++ src/main/java/com/project/hippohippogo/controllers/SuggestionsController.java
@@ -5,6 +5,7 @@ import com.project.hippohippogo.repositories.QueriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ public class SuggestionsController {
 
     @GetMapping("/suggestions")
     public String getSuggestions(@RequestParam("query") String query) throws JSONException {
-        List<SearchQuery> searchQueries = queriesRepository.findAll();
+        List<SearchQuery> searchQueries = queriesRepository.findAll(Sort.by(Sort.Direction.DESC, "hits"));
         ArrayList<String> queriesStrings = new ArrayList<>();
         String re = "^" + query.toLowerCase();
         Pattern pattern = Pattern.compile(re, Pattern.CASE_INSENSITIVE);
