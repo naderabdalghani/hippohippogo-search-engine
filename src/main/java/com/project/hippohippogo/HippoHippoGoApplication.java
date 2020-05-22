1b03d40ef1c7e73acf309820073df4d06ccd34d8
Muhanad <muhanad.atef23@gmail.com>
2020-05-22 09:48:21 +0200

    Some Changes In Main

--- src/main/java/com/project/hippohippogo/HippoHippoGoApplication.java
+++ src/main/java/com/project/hippohippogo/HippoHippoGoApplication.java
@@ -6,15 +6,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.List;
+
 @SpringBootApplication
 public class HippoHippoGoApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(HippoHippoGoApplication.class, args);
         CrawlerService crawlerService = applicationContext.getBean(CrawlerService.class);
-        crawlerService.Crawl();
         RankerService rankerService = applicationContext.getBean(RankerService.class);
-        rankerService.rankPages();
-        rankerService.getURLs("football soccer");
+        List<Integer> pagesIDs = rankerService.getURLs("football soccer");
     }
 
+    private void initialize(CrawlerService crawlerService,RankerService rankerService) {
+        crawlerService.Crawl();
+        rankerService.rankPages();
+    }
 }
