afc77b67866fc2274717b5d5eef52f6e20ddced5
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-23 05:45:26 +0200

    Add Custom Query Function for Pages Repository

--- src/main/java/com/project/hippohippogo/repositories/PagesRepository.java
+++ src/main/java/com/project/hippohippogo/repositories/PagesRepository.java
@@ -1,9 +1,13 @@
 package com.project.hippohippogo.repositories;
 
 import com.project.hippohippogo.entities.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PagesRepository extends JpaRepository<Page, Integer> {
+    public List<Page> findAllByIdIn(List<Integer> id, Pageable pageable);
 }
