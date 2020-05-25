ef5444b4205cdea12162a5d4cf6a40d72612856e
Mahmood Mohamed <mmmacmp@gmail.com>
2020-05-26 01:39:15 +0200

    Add custom queries to be used in indexer

--- src/main/java/com/project/hippohippogo/repositories/PagesRepository.java
+++ src/main/java/com/project/hippohippogo/repositories/PagesRepository.java
@@ -2,8 +2,17 @@ package com.project.hippohippogo.repositories;
 
 import com.project.hippohippogo.entities.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface PagesRepository extends JpaRepository<Page, Integer> {
+
+    @Query(value = "SELECT content FROM pages", nativeQuery = true)
+    ArrayList<String> getWebPages();
+    @Query(value = "SELECT id FROM pages", nativeQuery = true)
+    ArrayList<Integer> getWebPagesIds();
 }
