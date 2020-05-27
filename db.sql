d6f5755d07620f5fc8479b7b808e27831c6d385e
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-05-27 05:53:09 +0200

    Add Latest Database Schema Modifications

--- db.sql
+++ db.sql
@@ -1,65 +1,84 @@
 create schema mydb collate utf8mb4_0900_ai_ci;
 
-create table dummy_items
-(
-    _id int auto_increment
-        primary key,
-    name varchar(100) null,
-    query varchar(255) not null
-);
-
 create table hibernate_sequence
 (
     next_val bigint null
 );
 
-create table mydb.page_rank
+create table images
 (
-	page varchar(255) not null
-		primary key,
-	`rank` double not null,
-	out_links int not null
-);
-
-
-create table mydb.pages
-(
-	id int auto_increment
-		primary key,
-	link longtext null,
-	title longtext null,
-	content longtext null,
-	length int null,
-	description varchar(255) null
-);
-
-
-
-create table mydb.pages_connection
-(
-	id int auto_increment
-		primary key,
-	referred varchar(100) null,
-	referring varchar(100) null
-);
-
-
-
-create table search_queries
-(
-    query varchar(255) not null
+    id int auto_increment
         primary key,
-    hits int default 1 not null
+    image_link longtext not null,
+    source_link longtext not null,
+    title longtext not null,
+    length int not null,
+    region varchar(2) null,
+    date_published date null
 );
 
-create table mydb.words
+create table images_words
 (
-	id bigint auto_increment
-		primary key,
-	word varchar(100) not null,
-	doc_id int not null,
-	index_of_word int not null
+    id int auto_increment
+        primary key,
+    word varchar(255) not null,
+    doc_id int not null,
+    index_of_word int not null
 );
 
+create table page_rank
+(
+    page varchar(255) not null
+        primary key,
+    `rank` double not null,
+    out_links int not null
+);
 
+create table pages
+(
+    id int not null
+        primary key,
+    content longtext not null,
+    link longtext not null,
+    length int not null,
+    title longtext not null,
+    description longtext not null,
+    region varchar(2) null,
+    date_published date null
+);
+
+create table pages_connection
+(
+    id int not null
+        primary key,
+    referred varchar(255) not null,
+    referring varchar(255) not null
+);
+
+create table users
+(
+    ip varchar(255) not null,
+    query varchar(255) not null,
+    hits int default 1 not null,
+    region varchar(255) not null,
+    person tinyint(1) null,
+    primary key (ip, query, region)
+);
+
+create table users_frequent_domains
+(
+    domain varchar(255) not null,
+    user_ip varchar(255) not null,
+    hits int default 1 not null,
+    primary key (domain, user_ip)
+);
+
+create table words
+(
+    id int not null
+        primary key,
+    word varchar(255) null,
+    doc_id int null,
+    index_of_word int null
+);
 
