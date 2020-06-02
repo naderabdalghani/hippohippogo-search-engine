c7b08c83253e6957ff81c5f86e808d7826e3ee7e
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-06-03 00:59:03 +0200

    Add Suggestions Native SQL Queries to Search Queries Repository

--- src/main/java/com/project/hippohippogo/repositories/QueriesRepository.java
+++ src/main/java/com/project/hippohippogo/repositories/QueriesRepository.java
@@ -1,10 +1,17 @@
 package com.project.hippohippogo.repositories;
 
-import com.project.hippohippogo.entities.SearchQuery;
+import com.project.hippohippogo.entities.Query;
+import com.project.hippohippogo.ids.QueryId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface QueriesRepository extends JpaRepository<SearchQuery, String> {
+import java.util.List;
 
+@Repository
+public interface QueriesRepository extends JpaRepository<Query, QueryId> {
+    @org.springframework.data.jpa.repository.Query(value = "SELECT query FROM queries WHERE ip = ?1 AND query REGEXP ?2 ORDER BY hits DESC LIMIT ?3", nativeQuery = true)
+    List<String> findPersonalSuggestions(String userIp, String pattern, int limit);
+
+    @org.springframework.data.jpa.repository.Query(value = "SELECT query FROM (SELECT query, SUM(hits) AS totalHits FROM queries WHERE query REGEXP ?1 GROUP BY query ORDER BY totalHits DESC LIMIT ?2) as Q", nativeQuery = true)
+    List<String> findGlobalSuggestions(String pattern, int limit);
 }
