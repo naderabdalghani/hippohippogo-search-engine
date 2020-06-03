f9bd2f90626f081c43616cb49f1c1e1ff5b4fbca
Mahmood Mohamed <mmmacmp@gmail.com>
2020-06-03 04:33:48 +0200

    Modified and added queries to Page Repository

--- src/main/java/com/project/hippohippogo/repositories/PagesRepository.java
+++ src/main/java/com/project/hippohippogo/repositories/PagesRepository.java
@@ -3,8 +3,10 @@ package com.project.hippohippogo.repositories;
 import com.project.hippohippogo.entities.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +15,12 @@ import java.util.List;
 public interface PagesRepository extends JpaRepository<Page, Integer> {
     public List<Page> findAllByIdIn(List<Integer> id, Pageable pageable);
 
-    @Query(value = "SELECT content FROM pages", nativeQuery = true)
+    @Query(value = "UPDATE pages SET indexed=TRUE  WHERE id =?1", nativeQuery = true)
+    @Modifying
+    @Transactional
+    void setWebPagesIndexed(int id);
+    @Query(value = "SELECT content FROM pages WHERE indexed=FALSE ", nativeQuery = true)
     ArrayList<String> getWebPages();
-    @Query(value = "SELECT id FROM pages", nativeQuery = true)
+    @Query(value = "SELECT id FROM pages WHERE indexed=FALSE", nativeQuery = true)
     ArrayList<Integer> getWebPagesIds();
 }
