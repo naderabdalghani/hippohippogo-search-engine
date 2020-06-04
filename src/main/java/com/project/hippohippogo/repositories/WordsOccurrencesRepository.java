049133a7874cc2b4ac8da02a7bb328947d4e5e0f
Muhanad <muhanad.atef23@gmail.com>
2020-06-04 18:40:46 +0200

    Adding Title & Header In TF Calculations

--- src/main/java/com/project/hippohippogo/repositories/WordsOccurrencesRepository.java
+++ src/main/java/com/project/hippohippogo/repositories/WordsOccurrencesRepository.java
@@ -5,9 +5,24 @@ import com.project.hippohippogo.entities.WordsOccurrences;
 import com.project.hippohippogo.ids.WordId;
 import com.project.hippohippogo.ids.WordOccId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WordsOccurrencesRepository extends JpaRepository<WordsOccurrences, WordOccId> {
+
+    // Return title count of word from doc
+    @Query(value = "SELECT titlecount FROM words_occurrences WHERE doc_id = ?1 and word = ?2", nativeQuery = true)
+    int getTitleCount(int doc_id,String word);
+
+    // Return list with word and distinct documents containing it
+    @Query(value = "SELECT headercount FROM words_occurrences WHERE doc_id = ?1 and word = ?2", nativeQuery = true)
+    int getHeaderCount(int doc_id,String word);
+
+    // Check if word is exist or not
+    @Query(value = "SELECT EXISTS(SELECT * from words_occurrences WHERE doc_id=?1 and word = ?2)", nativeQuery = true)
+    int isWordExists(int doc_id,String word);
 
 }
